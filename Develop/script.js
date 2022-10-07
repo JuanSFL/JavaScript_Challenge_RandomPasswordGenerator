02da319b2dcbd47014bd5cd0b3671c5dc62932d3
JuanSFL <molina98j@yahoo.com>
2022-10-07 19:59:45 -0400

    Began to work on JavaScript by setting variables and if/else statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  numbers='0123456789'
+  lower="abcdefghijklmnopqrstuvwxyz"
+  upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  symbols="!@#$%^&*()_+~`[]\{}|;:<>?"
+}
+
+numberOfCharacters = prompt("How many characters would you like in your password? Choose between 8-128.")
+if (numberOfCharacters < 8 || numberOfCharacters > 128){
+  alert ("Choose a valid number of characters.")
+} else {
+  alert ("Your password will be " + numberOfCharacters + " characters long.")
+}
+
+IncludeNumbers = confirm ("Do you want numbers in your password?");
+if (!IncludeNumbers){
+  alert("Your password will not include numbers.")
+} else {
+  alert ("Your password will include numbers.")
+}
+
+IncludeLowercase = confirm("Do you want lowercase letters in your password?");
+if (!IncludeLowercase) {
+  alert("Your password will NOT include lowercase letters.");
+}
+else {
+  alert("Your password will include lowercase letters.");
+}
+
+IncludeUppercase = confirm ("Do you want uppercase letters in your password?")
+if (!IncludeUppercase){
+  alert ("Your password will NOT include uppercase letters!")
+} else {
+  alert ("Your password willl include uppercase letters!")
+}
+
+IncludeSymbols = confirm ("Do you want symbols in your password?")
+if (!IncludeSymbols) {
+  alert ("Your password will NOT contain symbols!")
+} else {
+  alert ("Your password will contain symbols!")
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +53,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+alert(generateBtn)
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
